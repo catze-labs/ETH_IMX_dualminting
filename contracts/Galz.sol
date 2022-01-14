b7c025a2bc0cd04c532dd116ba4ea2ab6d253dfd
cybergalz <93660202+cybergalz@users.noreply.github.com>
2022-01-14 22:52:00 +0900

    Add files via upload

---
+++ contracts/Galz.sol
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: MIT
+
+/*
+
+╋╋╋╋╋╋╋┏┓╋╋╋╋╋╋╋╋╋╋╋╋┏┓
+╋╋╋╋╋╋╋┃┃╋╋╋╋╋╋╋╋╋╋╋╋┃┃
+┏━━┳┓╋┏┫┗━┳━━┳━┳━━┳━━┫┃┏━━━┓
+┃┏━┫┃╋┃┃┏┓┃┃━┫┏┫┏┓┃┏┓┃┃┣━━┃┃
+┃┗━┫┗━┛┃┗┛┃┃━┫┃┃┗┛┃┏┓┃┗┫┃━━┫
+┗━━┻━┓┏┻━━┻━━┻┛┗━┓┣┛┗┻━┻━━━┛
+╋╋╋┏━┛┃╋╋╋╋╋╋╋╋┏━┛┃
+╋╋╋┗━━┛╋╋╋╋╋╋╋╋┗━━┛
+
+*/
+
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./utils/Bytes.sol";
+import "./utils/Minting.sol";
+
+abstract contract GalzRandomizer {
+    function getTokenId(uint256 tokenId) public view virtual returns(string memory);
+}
+
+contract Galz is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    
+    event GalzRevealed(uint256 tokenId, string fileId); // Sending the event for offchain script to transform the right file
+    event PaymentComplete(address indexed to, uint16 nonce, uint16 quantity); // then mint
+    event Minted(address indexed to, uint256 id);
+    event Withdraw(uint amount);
+    
+    address randomizerAddress; // Approved randomizer contract
+    address galzAutomatImxAddress; // Approved galzAutomatEth contract
+    //address[] galzPartAddressList;
+    address public imx;
+
+    string public _tokenUri = "https://api-galz.cybergalznft.com/"; // Initial base URI
+
+    mapping (address => mapping (uint256 => uint256)) onchainMetadata;
+    
+    bool public contractLocked = false;
+
+    constructor(address _imx) ERC721("Galz", "Galz") {
+        _tokenIdCounter.increment(); // Making sure we start at token ID 1
+        imx = _imx;
+    }
+
+    function mintTransfer(address to) public returns(uint256) {
+        require(msg.sender == galzAutomatImxAddress, "Not authorized");
+        
+        GalzRandomizer tokenAttribution = GalzRandomizer(randomizerAddress);
+        
+        string memory realId = tokenAttribution.getTokenId(_tokenIdCounter.current());
+        uint256 mintedId =  _tokenIdCounter.current();
+        
+        //_safeMint(to, _tokenIdCounter.current());
+        emit PaymentComplete(to, uint16(mintedId), 1);
+        emit GalzRevealed(_tokenIdCounter.current(), realId);
+        _tokenIdCounter.increment();
+        return mintedId;
+    }
+    
+    // Change the galzAutomatEth address contract
+    function setgalzAutomatEthAddress(address newAddress) public onlyOwner { 
+        galzAutomatImxAddress = newAddress;
+    }
+
+    // Change the randomizer address contract
+    function setRandomizerAddress(address newAddress) public onlyOwner {
+        randomizerAddress = newAddress;
+    }
+    
+    function secureBaseUri(string memory newUri) public onlyOwner {
+        require(contractLocked == false, "Contract has been locked and URI can't be changed");
+        _tokenUri = newUri;
+    }
+    
+    function lockContract() public onlyOwner {
+        contractLocked = true;   
+    }
+
+    /*
+    mapping (uint256 => uint256) tokenIdToPart; //partBytokenId
+    mapping (address => tokenIdToPart) onchainMetadata;
+    */
+
+    /*
+    // Set authorized contract address for minting the ERC-721 token
+    function addGalzPartContract(address contractAddress) public onlyOwner {
+        galzPartAddressList.push(contractAddress);
+    }
+
+    // Set Onchain Metadata
+    function setOnchainMetadata(address contractAddress, uint256 tokenId, uint256 value) public {
+        bool partExists = false;
+        for(uint256 i = 0; i < galzPartAddressList.length; i++) {
+            if ( msg.sender == galzPartAddressList[i] ) {partExists = true;}
+        }
+        require(partExists, "Not Authorized");
+        onchainMetadata[contractAddress][tokenId] = value;
+    }
+
+    function getOnchainMetadata(address contractAddress, uint256 tokenId) public returns (uint256 value) {
+        return onchainMetadata[contractAddress][tokenId];
+    }
+    */
+    
+	/*
+	 * Helper function
+	 */
+	function tokensOfOwner(address _owner) external view returns (uint256[] memory) {
+		uint256 tokenCount = balanceOf(_owner);
+		if (tokenCount == 0) return new uint256[](0);
+		else {
+			uint256[] memory result = new uint256[](tokenCount);
+			uint256 index;
+			for (index = 0; index < tokenCount; index++) {
+				result[index] = tokenOfOwnerByIndex(_owner, index);
+			}
+			return result;
+		}
+	}
+
+    //imx part starts
+    modifier onlyIMX() {
+        require(msg.sender == imx, "Function can only be called by IMX");
+        _;
+    }
+
+    function setIMX(address _imx) external onlyOwner {
+        imx = _imx;
+    }
+
+    // this part is essential, the condition for IMX minting. Should be here with all IMX compatible contracts
+    function mintFor(
+        address user,
+        uint256 quantity,
+        bytes calldata mintingBlob
+    ) external onlyIMX {
+        require(quantity == 1, 'Mintable: invalid quantity');
+        (uint256 id, bytes memory blueprint) = Minting.split(mintingBlob);
+        _mintFor(user, id, blueprint);
+
+        emit Minted(user, id);
+    }
+
+    function _mintFor(
+        address user,
+        uint256 id,
+        bytes memory
+    ) internal {
+        _safeMint(user, id);
+    }
+    //imx part ends
+
+    /** OVERRIDES */
+    function _baseURI() internal view override returns (string memory) {
+        return _tokenUri;
+    }
+    
+	function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ contracts/GalzRandomizer.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+
+/*
+╋╋╋╋╋╋╋┏┓╋╋╋╋╋╋╋╋╋╋╋╋┏┓
+╋╋╋╋╋╋╋┃┃╋╋╋╋╋╋╋╋╋╋╋╋┃┃
+┏━━┳┓╋┏┫┗━┳━━┳━┳━━┳━━┫┃┏━━━┓
+┃┏━┫┃╋┃┃┏┓┃┃━┫┏┫┏┓┃┏┓┃┃┣━━┃┃
+┃┗━┫┗━┛┃┗┛┃┃━┫┃┃┗┛┃┏┓┃┗┫┃━━┫
+┗━━┻━┓┏┻━━┻━━┻┛┗━┓┣┛┗┻━┻━━━┛
+╋╋╋┏━┛┃╋╋╋╋╋╋╋╋┏━┛┃
+╋╋╋┗━━┛╋╋╋╋╋╋╋╋┗━━┛
+*/
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract GalzRandomizer is VRFConsumerBase, Ownable {
+  using SafeMath for uint256;
+
+	bytes32 internal keyHash;
+
+	uint256 internal fee;
+  uint256 vrfvalue;
+
+  address galzAddress; // Approved galz contract
+
+	uint256[] public randomResults; //keeps track of the random number from chainlink
+  uint256[] public myGalzId; //galz shuffled with vrf
+	uint256 public totalDraws = 0; //drawID is drawID-1!
+	string[] public ipfsProof; //proof list where the list participants is
+	mapping(bytes32 => uint256) public requestIdToDrawIndex;
+
+	event IPFSProofAdded(string proof);
+	event RandomRequested(bytes32 indexed requestId, address indexed roller);
+	event RandomLanded(bytes32 indexed requestId, uint256 indexed result);
+	event Winners(uint256 randomResult, uint256[] expandedResult);
+	event Winner(uint256 randomResult, uint256 winningNumber);
+
+  //setGalzAddress(address newAddress)
+  //getRandomNumber()
+  //setRandomNumber()
+  //shuffle(9999)
+  //getMyGalzId(id)
+
+	constructor(
+		address _vrfCoordinator,
+		address _linkToken,
+		bytes32 _keyHash,
+		uint256 _fee
+	) VRFConsumerBase(_vrfCoordinator, _linkToken) {
+		keyHash = _keyHash;
+		fee = _fee;
+	}
+
+	//you start by calling this function and having in IPFS the list of participants
+	function addContestData(string memory ipfsHash) external onlyOwner {
+		ipfsProof.push(ipfsHash);
+		emit IPFSProofAdded(ipfsHash);
+	}
+
+	/**
+	 * Requests randomness
+	 */
+	function getRandomNumber() external onlyOwner returns (bytes32 requestId) {
+		require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK in the contract");
+		requestId = requestRandomness(keyHash, fee);
+		emit RandomRequested(requestId, msg.sender);
+		requestIdToDrawIndex[requestId] = totalDraws;
+		vrfvalue = uint256(requestId);
+	}
+
+  // set random number from chainlink vrf
+  function setRandomNumber() external onlyOwner returns (bytes32 requestId) {
+		require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK in the contract");
+		requestId = requestRandomness(keyHash, fee);
+		emit RandomRequested(requestId, msg.sender);
+		requestIdToDrawIndex[requestId] = totalDraws;
+		return requestId;
+	}
+
+	/**
+	 * Callback function used by VRF Coordinator
+	 */
+	function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+		randomResults.push(randomness);
+		totalDraws++;
+		emit RandomLanded(requestId, randomness);
+	}
+
+  // shuffle galz's list with amount (ex.shuffle(9999))
+  function shuffle(uint256 numberArr) public onlyOwner {
+    for (uint256 i = 0; i < numberArr; i++) {
+      uint256 n = i + vrfvalue % (numberArr - i) + 1;
+      myGalzId[i] = n;
+    }
+  }
+
+  // get my galz's id
+  function getTokenId(uint256 _id) public view returns(uint256 result){
+    require(msg.sender == galzAddress, "Not authorized");
+    return myGalzId[_id];
+  }
+
+  // Change the Galz address contract
+  function setGalzAddress(address newAddress) public onlyOwner { 
+      galzAddress = newAddress;
+  }
+
+	//------ other things --------
+	function withdrawLink() external onlyOwner {
+		LINK.transfer(owner(), LINK.balanceOf(address(this)));
+	}
+}
